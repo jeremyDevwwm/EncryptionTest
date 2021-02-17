8f822c907f87fe811b38b6614307bc33c26c2e6d
jeremyDevwwm <56993287+jeremyDevwwm@users.noreply.github.com>
2021-02-17 14:18:29 +0100

    Add files via upload

--- EncryptionTest/src/com/opcr/jeremy/Main.java
+++ EncryptionTest/src/com/opcr/jeremy/Main.java
@@ -1,10 +1,19 @@
 package com.opcr.jeremy;
 
+import com.opcr.jeremy.utils.EncryptionClass;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        EncryptionClass encryptionClass = new EncryptionClass();
 
+        String encrypted = encryptionClass.Encrypt("neo.wider@gmail.com");
+        System.out.println(encrypted);
+
+        String decrypted = encryptionClass.Decrypt(encrypted);
+
+        System.out.println(decrypted);
 
     }
 }
